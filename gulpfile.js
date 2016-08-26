2bf5226cb5df0d34af9940e37a071e8d4a76c92d
jyp220 <jyp@hatiolab.com>
2016-08-26 17:33:59 +0900

    push ing…

--- gulpfile.js
+++ gulpfile.js
@@ -150,6 +150,9 @@ gulp.task('vulcanize', function() {
       inlineCss: true,
       inlineScripts: true
     }))
+    .on( "error", function( err ) {
+      console.log( err );
+    })
     .pipe(gulp.dest(dist('elements')))
     .pipe($.size({title: 'vulcanize'}));
 });
